b689865de1ad348336ba0d85d58f7164d83af089
Ousama FakherEldine <ousfd1998@gmail.com>
2024-06-03 21:15:05 +0300

    Add .gitignore and remove environment files from repository

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://esxydmcyqizdigydvqbg.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzeHlkbWN5cWl6ZGlneWR2cWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MzYyNTgsImV4cCI6MjAzMzAxMjI1OH0.22Wy8TfpIMceaipNcAmnVxNzmfNtivf_DLYV2qnltoU'
-};
